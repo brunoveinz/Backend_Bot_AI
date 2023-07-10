dbb0cf3dc0174636dd096464a6a1e7a6692c8bdd
Bruno Veloso <velosobruno61@gmail.com>
2023-07-10 17:28:47 -0400

    variables de entorno base de datos cambiada correctamente

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
-export const DB_NAME = process.env.BD_NAME || 'sistema';
-export const DB_PORT = process.env.BD_PORT ||  '3306';
+export const DB_NAME = process.env.DB_NAME || 'sistema';
+export const DB_PORT = process.env.DB_PORT ||  '3306';
 
 // PUERTO DE LA APLICACION
 export const PORT = process.env.PORT || '3000';
